4b46fff8dd70e4cf7705df10c98dace4758e57e3
tutrunghieu <tutrunghieu@gmail.com>
2016-03-30 21:02:02 +0700

    Update files

--- multigradie-101/src-mar30-reconstr-test/apps/testers/poistesterConsts.java
+++ multigradie-101/src-mar30-reconstr-test/apps/testers/poistesterConsts.java
@@ -1,20 +1,24 @@
 package apps.testers;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class poistesterConsts {
 
 	public static boolean DEBUG = true;
 
-	public static List<File> getImageFiles() {
-		// TODO Auto-generated method stub
-		return null;
+	public static List<File> getImageFiles()
+	{
+		List<File> res = new ArrayList<File>();
+		res.add(new File("C:/Users/henrytu/Desktop/PossionImageEditing/image1.jpg"));
+		res.add(new File("C:/Users/henrytu/Desktop/PossionImageEditing/image2.jpg"));		
+		return res;
 	}
 
-	public static File getTable1() {
-		// TODO Auto-generated method stub
-		return null;
+	public static File getTable1()
+	{
+		return new File("C:/Users/henrytu/Desktop/temp-mar30/out.html");
 	}
 
 }
