5abf71e31c760165d44abeaa3f76d1f91718227e
tutrunghieu <tutrunghieu@gmail.com>
2016-03-30 21:16:41 +0700

    Removing debug

--- multigradie-101/src-mar30-reconstr-test/ds/gradies/PoisEditorJacobi.java
+++ multigradie-101/src-mar30-reconstr-test/ds/gradies/PoisEditorJacobi.java
@@ -2,8 +2,6 @@ package ds.gradies;
 
 import java.awt.image.BufferedImage;
 
-import apps.testers.poistesterConsts;
-
 public class PoisEditorJacobi extends PoisEditor {
 
 	public static int MAX_TIME = 5000;
@@ -11,8 +9,6 @@ public class PoisEditorJacobi extends PoisEditor {
 	@Override
 	public BufferedImage reconstructImage(BufferedImage ik, Mask mk) 
 	{
-		//if(poistesterConsts.DEBUG) return ik;
-		
 		VectorImage U = PoisEditor.outerColor(ik, mk);
 		VectorImage G = PoisEditor.innerGrad(ik, mk);
 		VectorImage R = PoisEditor.jacobi(U, G, mk, MAX_TIME );
